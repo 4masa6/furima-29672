de1a9253210eb380dd15cc86891d6b7c9900d8b7
4masa6 <ogamasa4466@gmail.com>
2020-09-23 20:30:14 +0900

    商品削除機能　修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: :new
-  before_action :move_to_index, except: [:index, :show, :new]
+  before_action :move_to_index, except: [:index, :show, :new, :destroy]
 
   def index
     @items = Item.all.order("created_at DESC")
@@ -15,7 +15,6 @@ class ItemsController < ApplicationController
   end
 
   def create
-    # binding.pry
     @item = Item.create(item_params)
     if @item.save
       redirect_to root_path
@@ -25,9 +24,11 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    item = Item.find(params[:id])
-    item.destroy
-    redirect_to root_path
+    if @item.destroy
+      redirect_to root_path
+    else
+      render :show
+    end
   end
   
   private
