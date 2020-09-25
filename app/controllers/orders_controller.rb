b594844597e5239e157f28c2cce3e9226ee4710d
4masa6 <ogamasa4466@gmail.com>
2020-09-25 21:10:33 +0900

    ルーティングの設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,11 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!, only:[ :index]
 
   def index
-    @item = Item.find_by(params[:id])
+    @item = Item.find(params[:item_id])
   end
   
+  private
+
+
 end
