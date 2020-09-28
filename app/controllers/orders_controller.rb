196b73b392afba15119acf8b922cf91208740c1c
4masa6 <ogamasa4466@gmail.com>
2020-09-28 19:14:55 +0900

    クレジットカード購入機能実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -9,9 +9,32 @@ class OrdersController < ApplicationController
   end
 
   def create
-    
+    @item = Item.find(params[:item_id])
+    @order = OrderAddress.new(order_params)
+    binding.pry
+    if @order.valid?
+      pay_item
+      @order.save
+      return redirect_to root_path
+    else
+      render "index"
+    end
   end
   
   private
+
+  def order_params
+    params.permit(:token, :item_id, :postal_code, :prefecture_id, :city, :address, :building, :phone_number).merge(user_id: current_user.id)
+  end
+
+  def pay_item
+    @item = Item.find(params[:item_id])
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.item_price,
+      card: order_params[:token],
+      currency:'jpy'
+    )
+  end
 
 end
